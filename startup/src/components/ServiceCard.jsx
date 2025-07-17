7d7b529cb1485df0bcaec51186c6d1bbe2f5db82
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-17 13:15:43 +0500

    add

--- startup/src/components/ServiceCard.jsx
+++ startup/src/components/ServiceCard.jsx
@@ -14,27 +14,30 @@ const ServiceCard = ({ service, onSelect, isSelected }) => {
         </div>
         <div className="card-text">
           <div className="card-header">
-          <h2 className="card-title">{service.title}</h2>
-          <span className={`status-badge ${service.status}`}>
-            {service.status === "active" && "Faol"}
-            {service.status === "busy" && "Band"}
-            {service.status === "inactive" && "Nofaol"}
-            {service.status === "available" && "Mavjud"}
-          </span>
-        </div>
-        </div>
-        <p className="card-description">{service.description}</p>
-        <div className="card-footer">
-          <div className="rating">
-            {[...Array(5)].map((_, i) => (
-              <FaStar key={i} className={i < service.rating ? "filled" : ""} />
-            ))}
+            <h2 className="card-title">{service.title}</h2>
+            <span className={`status-badge ${service.status}`}>
+              {service.status === "active" && "Faol"}
+              {service.status === "busy" && "Band"}
+              {service.status === "inactive" && "Nofaol"}
+              {service.status === "available" && "Mavjud"}
+            </span>
+            {/* <p className="card-description">{service.description}</p> */}
+            <div className="card-footer">
+              <div className="rating">
+                {[...Array(5)].map((_, i) => (
+                  <FaStar
+                    key={i}
+                    className={i < service.rating ? "filled" : ""}
+                  />
+                ))}
+              </div>
+              <button className="order-btn">Buyurtma</button>
+            </div>
           </div>
-          <button className="order-btn">Buyurtma</button>
         </div>
       </div>
     </div>
   );
 };
 
-export default ServiceCard
+export default ServiceCard;
