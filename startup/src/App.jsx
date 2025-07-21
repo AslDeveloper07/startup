adfc251beb3a301ce54063e890801adb61258065
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-21 16:48:35 +0500

    adda

--- startup/src/App.jsx
+++ startup/src/App.jsx
@@ -1,8 +1,8 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home, { DetailistPage } from './components/Home';
 import MapView from './components/MapView';
 import ProfilePage from './components/ProfilePage';
+import { DetailistPage, Home } from './components/Home';
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<MapView />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/detailist" element={<DetailistPage/>}
+        <Route path="/detailist" element={<DetailistPage/>}/>
       </Routes>
     </Router>
   );
